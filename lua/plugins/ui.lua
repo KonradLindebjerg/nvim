46e172caee87aea5409a3e56dffb9109cd1e5443
konlar-uf <konrad.lindebjerg@unitedfintech.com>
2026-06-04 10:35:19 +0200

    Fixed issue with Telescope pointing to wrong version creating issues when searching

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -12,7 +12,7 @@ return {
   -- Fuzzy Finder (Telescope)
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
+    branch = 'master',
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
